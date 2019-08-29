80c42e1473a9d8b03a3f6213dce1d2d9aefc36e8
Aidan McKay <aidanmckay2000@gmail.com>
2019-08-29 09:15:37 -0600

    fetch purchased data and set to state

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      purchases: [],
+      error: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/purchases')
+    .then(res => res.json())
+    .then(purchases => this.setState({ purchases }))
+    .catch(error => this.setState({error: 'There was an issue gathering your purchases'}))
+  }
   render() {
+    console.log(this.state.purchases);
+    
     return (
       <div className="App">
         <header>
