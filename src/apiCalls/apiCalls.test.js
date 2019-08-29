02bf6c9ec16c0edec630c708e2b9c76f9ef0cb67
Aidan McKay <aidanmckay2000@gmail.com>
2019-08-29 12:01:01 -0600

    begin apiCalls testing

---
+++ src/apiCalls/apiCalls.test.js
@@ -0,0 +1,11 @@
+import { fetchPurchases, postPurchases, deletePurchases } from './apiCalls';
+
+describe('Api Calls', () => {
+  const mockResponse = {
+    id: 1,
+    name: 'harry',
+    description:' hairy fella',
+    price: 1,
+    imgUrl: 'something'
+  }
+});
