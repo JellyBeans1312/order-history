ff41f559051cbe7eacbfccc061a993940efe87a9
Aidan McKay <aidanmckay2000@gmail.com>
2019-08-29 09:28:41 -0600

    display purchases on dom with no styling

--- src/CardContainer/CardContainer.js
+++ src/CardContainer/CardContainer.js
@@ -3,10 +3,16 @@ import Card from '../Card/Card'
 
 const CardContainer = ({ purchases }) => {
   const cards = purchases.map(purchase => {
-    return <Card purchase={...purchase} />
+    return <Card 
+        img={purchase.img}
+        name={purchase.name}
+        description={purchase.description}
+        price={purchase.price}
+        key={purchase.id}
+      />
   })
   return (
-    {cards}
+    cards
   )
 }
 
