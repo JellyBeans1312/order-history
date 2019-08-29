f65e318d38b172d5fb5b4cb61c6a9abbca0e8fe0
Aidan McKay <aidanmckay2000@gmail.com>
2019-08-29 11:51:45 -0600

    added methods for deleting and posting purchase

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -10,3 +10,41 @@ export const fetchPurchases = () => {
       throw Error(error.message);
     })
 }
+export const deletePurchases = (id) => {
+  const options = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  return fetch(`http://localhost:3001/api/v1/purchases/${id}`, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error deleting purchase');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      throw Error(error.message);
+    })
+}
+
+export const postPurchases = (newPurchase) => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(newPurchase),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  return fetch('http://localhost:3001/api/v1/purchases', options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error adding new purchase');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      throw Error(error.message);
+    })
+}
